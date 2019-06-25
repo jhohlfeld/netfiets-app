bad1eaf7071f768b8221bd49ccfce81f2be36c59
Jakob Hohlfeld <jhohlfeld@polyban.io>
2019-06-25 21:29:48 +0200

    fix: revert firebase config publication

--- firebase.config.ts
+++
@@ -1,7 +0,0 @@
-export default {
-  apiKey: "AIzaSyCn4eRK_lzZ3KzX40d4r15OpcdB0MDM6eQ",
-  authDomain: "netfiets-app.firebaseapp.com",
-  databaseURL: "https://netfiets-app.firebaseio.com",
-  projectId: "netfiets-app",
-  appId: "1:436450621864:web:e734bebbef31681d"
-}
