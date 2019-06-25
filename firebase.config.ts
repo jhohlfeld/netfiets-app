786f43102d94ac64c735423bddbd647bf463208e
Jakob Hohlfeld <jhohlfeld@polyban.io>
2019-06-25 21:34:51 +0200

    add firebase config again since it's safe

---
+++ firebase.config.ts
@@ -0,0 +1,7 @@
+export default {
+  apiKey: "AIzaSyCn4eRK_lzZ3KzX40d4r15OpcdB0MDM6eQ",
+  authDomain: "netfiets-app.firebaseapp.com",
+  databaseURL: "https://netfiets-app.firebaseio.com",
+  projectId: "netfiets-app",
+  appId: "1:436450621864:web:e734bebbef31681d"
+}
